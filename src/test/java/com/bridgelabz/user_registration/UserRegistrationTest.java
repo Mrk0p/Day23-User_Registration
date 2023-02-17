d072b556143f8b1fbce6daea0e49777108977d01
Mrk0p <sachinwele23@gmail.com>
2023-02-17 11:52:04 +0530

    Added:test valid first name

---
+++ src/test/java/com/bridgelabz/user_registration/UserRegistrationTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.user_registration;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+    UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+    public void givenFirstName_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Rajkumar");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_ShouldReturnFalse() {
+        boolean result = userRegistration.firstName("rajkumar");
+        Assert.assertEquals(false, result);
+    }
+}
